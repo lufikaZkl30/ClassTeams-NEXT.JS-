d816961e31f8bf5af1c91f44610c2bf9f5c3c624
LufikaZkl <lufikabgy337@gmail.com>
2026-05-02 03:05:41 +0700

    add

--- classteams-next/app/page.tsx
+++ classteams-next/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Breadcrumb from "@/components/Breadcrumb";
